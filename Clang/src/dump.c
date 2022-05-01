36baa18582386327d72ada4e36cdd81ee5f59dda
gm <37975692+puppies-jp@users.noreply.github.com>
2022-05-01 16:28:32 +0900

    色々追加

--- Clang/src/dump.c
+++ Clang/src/dump.c
@@ -1,21 +1,3 @@
-# ここでは C/C++ 系の言語についてのあれこれをまとめる
-
-- [Cmake について](Cmake)
-- [コンストラクト/デストラクト](construct)
-- [mutex の安全な運用について](mutex)
-- [プリプロセッサ使い方 cheat メモ](preprocessor)
-- [C++の安全なリソース解放について](SafeResource)
-- [引数について](FunctionParameter)
-
----
-
-- よく使うソース群
-  - [メモリダンプ](#memory_dump)
-
-
-## <a name=memory_dump>メモリダンプ</a>
-
-```c
 #include "stdio.h"
 
 /*
@@ -62,4 +44,3 @@ int main()
     return 0;
 }
 */
-```
