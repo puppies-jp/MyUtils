09031c417f18c97e0c2cd55eb5447c1433756be9
gm <37975692+puppies-jp@users.noreply.github.com>
2022-09-09 01:08:14 +0900

    add readmemory ad opencv

--- Clang/src/opengl/test2.cpp
+++ Clang/src/opengl/test2.cpp
@@ -1,7 +1,12 @@
-#include <GLUT/glut.h>
 #include <stdio.h>
 #include <vector>
 #include <iostream>
+#include <cstdlib>
+
+#include "../dump.h"
+#include <GLUT/glut.h>
+#include <opencv2/opencv.hpp>
+
 /*
  * g++ test.cpp -o sample01.out -Wall -framework GLUT -framework OpenGL
  */
@@ -11,6 +16,7 @@
 float vertex[ARRAY_MAX];
 int lines[ARRAY_MAX];
 int vertexDataSize = 0, lineDataSize = 0;
+int width = 500, height = 500;
 
 void disp()
 {
@@ -57,7 +63,18 @@ void disp()
         }
     }
     glEnd();
+    // 取得画像エリアの確保(width*height)
+    unsigned char *buffer =
+        (unsigned char *)malloc(width * height * 4 * sizeof(unsigned char));
+    // フロントを読み込む様に設定する
+    glReadBuffer(GL_FRONT);
+    // 描画内容の読込
+    glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
+    // cv::Mat image = cv::Mat::zeros(500, 500, CV_8UC3);
+    // cv::imwrite("output.png", image);
     glFlush();
+    dump(buffer, width * height * 4 * sizeof(char));
+    free(buffer);
 }
 
 void timer(int x)
@@ -91,7 +108,7 @@ int main(int argc, char **argv)
     InitialProc();
 
     glutInit(&argc, argv);
-    glutInitWindowPosition(50, 100);
+    glutInitWindowPosition(100, 10);
     glutInitWindowSize(500, 500);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
 
@@ -101,8 +118,9 @@ int main(int argc, char **argv)
     glRotatef(30, 0, 1.0, 0);
     glScalef(0.8, 0.8, 0.8);
 
-    glutDisplayFunc(disp);
-    glutTimerFunc(100, timer, 0);
+    disp();
+    // glutDisplayFunc(disp);
+    // glutTimerFunc(100, timer, 0);
 
     glutMainLoop();
 
